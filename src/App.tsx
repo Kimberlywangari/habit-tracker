ab85eda861e238aa77d4e9fa2234e7f167787f90
Kimberly Wangari <kimberlywangarike@gmail.com>
2026-08-31 14:54:02 +0300

    set-up done and configured tailwindcss to the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return <div>
+<Header></Header>
+
+  </div>
+}
+function Header() {
+  return (
+    <header>
+      <h1>Habit Tracker</h1>
+    </header>
+  )
+}
