c25a76e25d986ba1d37b47ab5757bb6e9e853daf
Kimberly Wangari <kimberlywangarike@gmail.com>
2026-09-01 10:15:48 +0300

    button custom component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { Button } from '../components/Button'
 export default function App() {
-  return <div>
+  return <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
 <Header></Header>
 
   </div>
 }
 function Header() {
   return (
-    <header>
-      <h1>Habit Tracker</h1>
+    <header className="flex items-center justify-between">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl font-bold">Habit Tracker</h1>
+        <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+      </div>
+       <div className="flex flex-col gap-2 items-end">
+        <span className="text-zinc-400 text-sm">Sep 1 - Sep 7</span>
+        <div className="flex items-center gap-2">
+          <Button>Prev</Button>
+          <Button>Next</Button>
+          
+        </div>
+      </div>
+      
+
     </header>
   )
 }
