b8400dcbad6cf10706fd9bf316977e22a559e288
Kimberly Wangari <kimberlywangarike@gmail.com>
2026-09-01 10:32:08 +0300

    completed the Header component

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import { Button } from './Button'
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-between">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl font-bold">Habit Tracker</h1>
+        <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+      </div>
+       <div className="flex flex-col gap-2 items-end">
+        <span className="text-zinc-400 text-sm">Sep 1 - Sep 7</span>
+        <div className="flex items-center gap-2">
+          <Button>Prev</Button>
+          <Button>Next</Button>
+          
+        </div>
+      </div>
+      
+
+    </header>
+  )
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import { Button } from '../components/Button'
 export default function App() {
   return <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
-<Header></Header>
+<Header />
+<HabitForm />
 
   </div>
 }
-function Header() {
-  return (
-    <header className="flex items-center justify-between">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-3xl font-bold">Habit Tracker</h1>
-        <span className="text-zinc-400 text-sm">1 / 1 done today</span>
-      </div>
-       <div className="flex flex-col gap-2 items-end">
-        <span className="text-zinc-400 text-sm">Sep 1 - Sep 7</span>
-        <div className="flex items-center gap-2">
-          <Button>Prev</Button>
-          <Button>Next</Button>
-          
-        </div>
-      </div>
-      
 
-    </header>
+function HabitForm() {
+  return (
+   <div> habit form</div>
   )
 }
